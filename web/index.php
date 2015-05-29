ace1f19e1427a141656b5ba804328b82f791ac13
Morgan Pyne <morgan@vendhq.com>
2015-05-30 01:40:44 +1200

    Fixes timezone/time support.
    Fixes the way timezones are interpreted from the PagerDuty API,
    and re-enables the ability to have the on-call persons's time
    announced (now a configurable flag)

--- web/index.php
+++ web/index.php
@@ -18,6 +18,11 @@ $scheduleID = getenv('PAGERDUTY_SCHEDULE_ID');
 $APItoken   = getenv('PAGERDUTY_API_TOKEN');
 $domain     = getenv('PAGERDUTY_DOMAIN');
 
+// Should we announce the local time of the on-call person?
+// (helps raise awareness you might be getting somebody out of bed)
+$announceTime = getenv('PHONEDUTY_ANNOUNCE_TIME');
+
+
 $pagerduty = new \Vend\Phoneduty\Pagerduty($APItoken, $domain);
 
 $userID = $pagerduty->getOncallUserForSchedule($scheduleID);
@@ -30,10 +35,16 @@ if (null !== $userID) {
         'language' => 'en-GB'
     );
 
+    $time = "";
+    if ($announceTime && $user['local_time']) {
+        $time = sprintf("The current time in their timezone is %s.", $user['local_time']->format('g:ia'));
+    }
+
     $twilioResponse = new Services_Twilio_Twiml();
-    $response = sprintf("The current on-call engineer is %s. "
+    $response = sprintf("The current on-call engineer is %s. %s "
         . "Please hold while we connect you.",
-        $user['first_name']
+        $user['first_name'],
+        $time
         );
 
     $twilioResponse->say($response, $attributes);
