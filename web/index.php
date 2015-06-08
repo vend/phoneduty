2c26ebd0fd74eac519ce28cc94d36acc7a8be8cf
jvleonor <joshua.leonor@hautelook.com>
2015-06-08 16:04:21 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -48,7 +48,7 @@ if (null !== $userID) {
         );
 
     $twilioResponse->say($response, $attributes);
-    $twilioResponse->dial callerId="+12132236055"( $user['phone_number'], $attributes);
+    $twilioResponse->dial( array("callerId" => "+15551112222"), $user['phone_number'], $attributes);
 
     // send response
     if (!headers_sent()) {
