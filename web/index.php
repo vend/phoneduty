e5fb4733e977a9dfaca33bc3c8bdd078824acbe0
Matt Collins <mcollins@pagerduty.com>
2016-05-31 14:26:09 -0700

    Testing out an optional human verification prompt.

--- web/index.php
+++ web/index.php
@@ -19,6 +19,7 @@ $APItoken        = getenv('PAGERDUTY_API_TOKEN');
 $serviceAPItoken = getenv('PAGERDUTY_SERVICE_API_TOKEN');
 $domain          = getenv('PAGERDUTY_DOMAIN');
 $greeting        = getenv('PHONEDUTY_ANNOUNCE_GREETING');
+$validate_human  = getenv('PHONEDUTY_CHECK_FOR_HUMAN');
 
 // Should we announce the local time of the on-call person?
 // (helps raise awareness you might be getting somebody out of bed)
@@ -53,28 +54,43 @@ if ($greeting != '') {
     $twilio->say(sprintf("%s", $greeting), $attributes);
 }
 
-if ($user !== null) {
-    $time = "";
-    if (($announceTime == 'true' || $announceTime == 'True') && $user['local_time']) {
-        $time = sprintf(" The current time in their timezone is %s.", $user['local_time']->format('g:ia'));
+if (isset($_POST['Digits'])) {
+    if ($_POST['Digits'] != '') {
+        $_SESSION['end_user_confirmed_call'] = true;
     }
+}
 
-    $twilio->say(sprintf("The current on-call engineer is %s." .
-        "%s Please hold while we connect you.",
-        $user['first_name'], $time), $attributes);
-    
-    $dialvars = array('action' => "check_if_completed_by_human.php", 'timeout' => 25);
-    if ($record != strtolower("false")) {
-        if ($record == strtolower("true")) {
-            $dialvars['record'] = "true";
-        } else {
-            $dialvars['record'] = $record;
+if (!$_SESSION['end_user_confirmed_call'] and $validate_human == strtolower("true")) {
+    $gather = $twilio->gather(array('timeout' => 25, 'numDigits' => 1));
+    $gather->say("Press 1 to reach the on-call engineer.", $attributes);
+    $twilio->say("Goodbye.", $attributes);
+    $twilio->hangup();
+}
+
+if ($_SESSION['end_user_confirmed_call'] or $validate_human == strtolower("false")) {
+    if ($user !== null) {
+        $time = "";
+        if ($announceTime == strtolower("true") && $user['local_time']) {
+            $time = sprintf(" The current time in their timezone is %s.", $user['local_time']->format('g:ia'));
         }
+
+        $twilio->say(sprintf("The current on-call engineer is %s." .
+            "%s Please hold while we connect you.",
+            $user['first_name'], $time), $attributes);
+
+        $dialvars = array('action' => "check_if_completed_by_human.php", 'timeout' => 25);
+        if (!$record != strtolower("false")) {
+            if ($record == strtolower("true")) {
+                $dialvars['record'] = "true";
+            } else {
+                $dialvars['record'] = $record;
+            }
+        }
+        $dial = $twilio->dial(NULL, $dialvars);
+        $dial->number($user['phone_number'], array('url' => "check_for_human.php"));
+    } else {
+        $twilio->redirect('voicemail.php');
     }
-    $dial = $twilio->dial(NULL, $dialvars);
-    $dial->number($user['phone_number'], array('url' => "check_for_human.php"));
-} else {
-    $twilio->redirect('voicemail.php');
 }
 
 // send response
