3fa951e29fc733aad160b4f0db0828263bdff6a6
Matt Collins <mcollins@pagerduty.com>
2016-05-31 16:18:20 -0700

    GG me...

--- web/index.php
+++ web/index.php
@@ -60,7 +60,7 @@ if (isset($_POST['Digits'])) {
     }
 }
 
-error_log($_SESSION['end_user_confirmed_call'] . " " . $validate_human)
+error_log($_SESSION['end_user_confirmed_call'] . " " . $validate_human);
 
 if (!$_SESSION['end_user_confirmed_call']){
     if ($validate_human == strtolower("true")) {
